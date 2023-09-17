bdee9d0a02071b9908da03a89c60d5caa85946ac
Nobuchika Tanaka <codequokka@gmail.com>
2023-09-18 05:36:22 +0900

    Refactor hello_test.go

--- hello-world/hello_test.go
+++ hello-world/hello_test.go
@@ -3,9 +3,21 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	t.Run("say hello to people", func(t *testing.T) {
+	t.Run("say hello to people in English", func(t *testing.T) {
 		got := Hello("Chris", "")
 		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello to people in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("say hello to people in French", func(t *testing.T) {
+		got := Hello("Lauren", french)
+		want := "Bonjour, Lauren"
 		assertCorrectMessage(t, got, want)
 	})
 
@@ -15,17 +27,6 @@ func TestHello(t *testing.T) {
 		assertCorrectMessage(t, got, want)
 	})
 
-	t.Run("say hello to people in Spanish", func(t *testing.T) {
-		got := Hello("Elodie", "Spanish")
-		want := "Hola, Elodie"
-		assertCorrectMessage(t, got, want)
-	})
-
-	t.Run("say hello to people in French", func(t *testing.T) {
-		got := Hello("Lauren", "French")
-		want := "Bonjour, Lauren"
-		assertCorrectMessage(t, got, want)
-	})
 }
 
 func assertCorrectMessage(t *testing.T, got string, want string) {
