e5b5f5dc5423758f9e890003f7a06974442861ef
Nobuchika Tanaka <codequokka@gmail.com>
2023-09-18 13:42:24 +0900

    Add ExampleRepeat()

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -1,6 +1,9 @@
 package iteration
 
-import "testing"
+import (
+	"fmt"
+	"testing"
+)
 
 func TestRepeat(t *testing.T) {
 	repeated := Repeat("a", 3)
@@ -9,6 +12,12 @@ func TestRepeat(t *testing.T) {
 	if repeated != expected {
 		t.Errorf("expected %q but got %q", expected, repeated)
 	}
+}
+
+func ExampleRepeat() {
+	repeated := Repeat("a", 4)
+	fmt.Println(repeated)
+	// Output: aaaa
 }
 
 func BenchmarkRepeat(b *testing.B) {
